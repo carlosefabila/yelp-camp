53bdf1993bb7c69fc5a4ed61e055e8c006bd82cb
carlosefabila <57574536+carlosefabila@users.noreply.github.com>
2024-02-05 23:07:30 +0100

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp")
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((err) => {
+        console.log("Ohh no!! MongoDB connection Error!!");
+        console.log(err);
+    })
+
 const app = express();
 const port = 3000;
-const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -9,6 +21,12 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+})
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({title: "My Backyard", description: "Cheap Camping."});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
