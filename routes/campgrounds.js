d72e7b5718d42aa44cc421495d5518b80a24d130
carlosefabila <57574536+carlosefabila@users.noreply.github.com>
2024-02-29 14:05:09 +0100

    Upload campground imge to Cloudinary from New View

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn, isAuthor, validateCampground } = require("../utils/middleware");
 const campgrounds = require("../controllers/campgrounds");
+const multer  = require('multer')
+const { storage } = require("../cloudinary");
+const upload = multer({ storage })
 
 router.route("/")
     .get(campgrounds.index)
-    .post(isLoggedIn, validateCampground, campgrounds.createCampground);
+    // .post(isLoggedIn, validateCampground, campgrounds.createCampground);
+    .post(upload.single("image"), (req, res) => {
+        console.log(req.body, req.file);
+        res.send(req.body);
+    })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
